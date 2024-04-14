bf05a7bbfc760eabd3ba281241fb3eac8c58bffa
Tommy <contact@tommytran.io>
2024-04-14 01:08:28 -0700

    Typo Fix
    Signed-off-by: Tommy <contact@tommytran.io>

--- Scripts/DeBloat.ps1
+++ Scripts/DeBloat.ps1
@@ -1,4 +1,4 @@
 $Bloat = 'MicrosoftTeams|Microsoft.ScreenSketch|Microsoft.Paint|Microsoft.549981C3F5F10|Microsoft.MicrosoftOfficeHub|Microsoft.WindowsFeedbackHub|Microsoft.People|Clipchamp.Clipchamp|Microsoft.GetStarted|MicrosoftCorporationII.QuickAssist|Microsoft.WindowsAlarms|Microsoft.WindowsSoundRecorder|Microsoft.YourPhone|Microsoft.WindowsMaps|Microsoft.MicrosoftSolitaireCollection|Microsoft.Windows.DevHome|Microsoft.OutlookForWindows'
 
-Get-AppxPackage -AllUsers | Where-Object {$_.Name -Match $Bloat} | Remove-AppxPackage
+Get-AppxPackage -AllUsers | Where-Object {$_.Name -Match $Bloat} | Remove-AppxPackage -AllUsers
 mstsc /uninstall
